9f48b6c0b7ee5635b53087663fa2545214680e2b
chenbinios <296776627@qq.com>
2025-05-23 17:36:31 +0800

    1

---
+++ langchain-assistant/langchain-learn/1_langchain_commu_ollama.py
@@ -0,0 +1,15 @@
+
+# langchain和本地的ollama服务进程通信
+
+from langchain_ollama import OllamaLLM
+from langchain.prompts import PromptTemplate
+
+
+llm = OllamaLLM(model="llama3", temperature=0.7,stream=False)
+
+prompt = PromptTemplate.from_template("请用中文回答：{question}")
+
+chain =  prompt | llm
+
+response = chain.invoke({"question":"What is the capital of France?"})
+print(response)
